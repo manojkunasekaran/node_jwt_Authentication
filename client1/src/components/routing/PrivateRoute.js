ffac855d545c7981c442cb66c7c964b9f94116c2
Manoj K <80871895+manojk89@users.noreply.github.com>
2022-09-14 09:51:38 +0530

    Add files via upload

---
+++ client1/src/components/routing/PrivateRoute.js
@@ -0,0 +1,20 @@
+// import { Component } from 'react';
+import React from "react";
+import {Navigate, Route} from 'react-router-dom';
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+    return (
+        <Route
+          {...rest}
+          render={(props) =>
+            localStorage.getItem("authToken") ? (
+              <Component {...props} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+      );
+};
+
+export default PrivateRoute;
